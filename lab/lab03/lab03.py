bfcc05be73e75baee1d33b2e4ec23638fccbc4a4
Your Name <you@example.com>
2019-09-27 01:04:49 +1000

    "Finished maps project, happy!"

---
+++ lab/lab03/lab03.py
@@ -0,0 +1,52 @@
+""" Lab 3: Recursion and Midterm Review """
+
+def gcd(a, b):
+    """Returns the greatest common divisor of a and b.
+    Should be implemented using recursion.
+
+    >>> gcd(34, 19)
+    1
+    >>> gcd(39, 91)
+    13
+    >>> gcd(20, 30)
+    10
+    >>> gcd(40, 40)
+    40
+    """
+    if a % b == 0:
+        return b
+
+    if b % a == 0:
+        return a
+
+    if a > b:
+        return gcd(b, a % b)
+
+    if b > a:
+        return gcd(a, b % a)
+def hailstone(n):
+    """Print out the hailstone sequence starting at n, and return the
+    number of elements in the sequence.
+
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    """
+    if n == 1:
+        print(n)
+        return 1
+    
+    if n % 2 == 0:
+        print(n)
+        return 1 + hailstone(n // 2)
+
+    if n % 2 == 1:
+        print(n)
+        return 1 + hailstone(3 * n + 1)
