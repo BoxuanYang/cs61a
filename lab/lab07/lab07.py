6783a770a188b509a2d9a85c975e0e092326ca4b
David Young <u6778195@anu.edu.au>
2020-04-25 11:46:46 +1000

    finished hw07

---
+++ lab/lab07/lab07.py
@@ -0,0 +1,160 @@
+""" Lab 07: Recursive Objects """
+
+
+# Q3
+def link_to_list(link):
+    """Takes a linked list and returns a Python list with the same elements.
+
+    >>> link = Link(1, Link(2, Link(3, Link(4))))
+    >>> link_to_list(link)
+    [1, 2, 3, 4]
+    >>> link_to_list(Link.empty)
+    []
+    """
+    if link is Link.empty:
+        return []
+    else:
+        l = [link.first]
+        link = link.rest
+        return l + link_to_list(link)
+
+
+# Q4
+def store_digits(n):
+    """Stores the digits of a positive number n in a linked list.
+
+    >>> s = store_digits(1)
+    >>> s
+    Link(1)
+    >>> store_digits(2345)
+    Link(2, Link(3, Link(4, Link(5))))
+    >>> store_digits(876)
+    Link(8, Link(7, Link(6)))
+    """
+    digits = [int(d) for d in str(n)]
+    link = Link(digits[0])
+    myL = link
+    i = 0
+    while i < len(digits):
+        if i == 0:
+            i += 1
+            continue
+        myL.rest = Link(digits[i])
+        myL = myL.rest
+        i = i + 1
+    return link
+
+
+# Q5
+def cumulative_sum(t):
+    """Mutates t where each node's root becomes the sum of all entries in the
+    corresponding subtree rooted at t.
+
+    >>> t = Tree(1, [Tree(3, [Tree(5)]), Tree(7)])
+    >>> cumulative_sum(t)
+    >>> t
+    Tree(16, [Tree(8, [Tree(5)]), Tree(7)])
+    """
+    sum = sum_tree(t)
+    t.label = sum
+    for tree in t.branches:
+        tree.label = sum_tree(tree)
+
+
+def sum_tree(tree):
+    if tree.is_leaf():
+        return tree.label
+    else:
+        sum = tree.label
+        for sub_tree in tree.branches:
+            sum += sum_tree(sub_tree)
+
+    return sum
+
+
+# Linked List Class
+class Link:
+    """A linked list.
+
+    >>> s = Link(1)
+    >>> s.first
+    1
+    >>> s.rest is Link.empty
+    True
+    >>> s = Link(2, Link(3, Link(4)))
+    >>> s.second
+    3
+    >>> s.first = 5
+    >>> s.second = 6
+    >>> s.rest.rest = Link.empty
+    >>> s                                    # Displays the contents of repr(s)
+    Link(5, Link(6))
+    >>> s.rest = Link(7, Link(Link(8, Link(9))))
+    >>> s
+    Link(5, Link(7, Link(Link(8, Link(9)))))
+    >>> print(s)                             # Prints str(s)
+    <5 7 <8 9>>
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    @property
+    def second(self):
+        return self.rest.first
+
+    @second.setter
+    def second(self, value):
+        self.rest.first = value
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
+
+
+# Tree Class
+class Tree:
+    def __init__(self, label, branches=[]):
+        for c in branches:
+            assert isinstance(c, Tree)
+        self.label = label
+        self.branches = list(branches)
+
+    def __repr__(self):
+        if self.branches:
+            branches_str = ', ' + repr(self.branches)
+        else:
+            branches_str = ''
+        return 'Tree({0}{1})'.format(self.label, branches_str)
+
+    def is_leaf(self):  # branches equal to []
+        return not self.branches
+
+    def __eq__(self, other):
+        return type(other) is type(self) and self.label == other.label \
+               and self.branches == other.branches
+
+    def __str__(self):
+        def print_tree(t, indent=0):
+            tree_str = '  ' * indent + str(t.label) + "\n"
+            for b in t.branches:
+                tree_str += print_tree(b, indent + 1)
+            return tree_str
+
+        return print_tree(self).rstrip()
+
+    def copy_tree(self):
+        return Tree(self.label, [b.copy_tree() for b in self.branches])
